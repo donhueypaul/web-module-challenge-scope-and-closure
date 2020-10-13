263ee3c501a3e27f425076004340cf4d85f8705f
donhueypaul <55775606+donhueypaul@users.noreply.github.com>
2020-10-12 21:25:08 -0700

    Solved 2 problems

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ * ccounter1 is a local scope and counter2 is a global scope
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ * counter1 because it is a function using variables from outside its scope
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ *If you only want to affect all variables outside of block use counter1
 */
 
 // counter1 code
@@ -56,7 +56,7 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(Math.random{) * 2);
 
     /*Code Here*/
 
